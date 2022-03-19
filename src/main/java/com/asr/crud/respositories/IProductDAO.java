029eee76b97f9e78b1d4e951380fe1213cc92e48
AdriAsencio <adriana.asencio.est@tecazuay.edu.ec>
2022-03-18 20:22:49 -0500

    trabajo api rest

---
+++ src/main/java/com/asr/crud/respositories/IProductDAO.java
@@ -0,0 +1,13 @@
+package com.asr.crud.respositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.asr.crud.models.ProductDTO;
+
+
+@Repository
+public interface IProductDAO extends MongoRepository<ProductDTO, String> {
+	
+	
+}
